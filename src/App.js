f2a0349b03a1dfba171d9bc1b84dd896d0532533
anda simion <andasimion@andas-Mac-mini.local>
2019-06-24 16:12:23 +0300

    Removed the render of WeatherForecastDisplay component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import WeatherForm from './components/WeatherForm';
-import WeatherForecastDisplay from './components/WeatherForecastDisplay';
 import './App.css';
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <div className="App">
       <p>Weather</p>
       <WeatherForm />
-      <WeatherForecastDisplay />
     </div>
   );
 }
